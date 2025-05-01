681afc50c8f52bc01ebc29272c05bbb7b21e2189
bernabaris <bernabaris1 @gmail.com>
2025-05-01 12:51:33 +0300

    init code

---
+++ src/test/java/com/github/bernabaris/mediachooser/MediaChooserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bernabaris.mediachooser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaChooserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
